c926f01c0f06f66dc90f2b0628ffc6da88746802
DonLofto <DonLofto@users.noreply.github.com>
2023-01-29 12:22:12 +0000

    Fixed not throwing exception

--- YourPrimeTest.java
+++ YourPrimeTest.java
@@ -12,27 +12,27 @@ public class YourPrimeTest {
 
 	public static void main(String[] args) {
 		// dummy songs
-		Song s1 = new Song("Fight The Power", "Public Enemy", 4, new Rap());
-		Song s2 = new Song("Smell Like Teen Spirit", "Nirvana", 4, new Rock());
-		Song s3 = new Song("Blinding Lights", "The Weeknd", 2, new Pop());
-		Song s4 = new Song("We Belong Together", "Mariah Carey", 3, new Pop());
-		Song s5 = new Song("Hey Jude", "The Beatles", 3, new Rock());
+		Song s1 = Song.of("Fight The Power", "Public Enemy", 4, new Rap());
+		Song s2 = Song.of("Smell Like Teen Spirit", "Nirvana", 4, new Rock());
+		Song s3 = Song.of("Blinding Lights", "The Weeknd", 2, new Pop());
+		Song s4 = Song.of("We Belong Together", "Mariah Carey", 3, new Pop());
+		Song s5 = Song.of("Hey Jude", "The Beatles", 3, new Rock());
 
 		// dummy movies
-		Movie m1 = new Movie("The Godfather", "Marlon Brando, Al Pacino", 175.0, 1972, 4, new Drama());
-		Movie m2 = new Movie("Avangers: Endgame", "Ironman, Capt. America, Thor", 181.0, 2019, 2, new Action());
-		Movie m3 = new Movie("Zoolander", "Ben Stiller, Owen Wilson", 90.0, 1998, 1, new Comedy());
-		Movie m4 = new Movie("The Shawshank Redemption", "Tim Robbins, Morgan Freeman", 142.0, 1994, 5, new Drama());
-		Movie m5 = new Movie("Forrest Gump", "Tom Hanks", 142.5, 1994, 3, new Comedy());
-		Movie m6 = new Movie("The Dark Knight", "Christian Bale, Heath Ledger", 152.0, 2008, 5, new Rock());
+		Movie m1 = Movie.of("The Godfather", "Marlon Brando, Al Pacino", 175.0, 1972, 4, new Drama());
+		Movie m2 = Movie.of("Avangers: Endgame", "Ironman, Capt. America, Thor", 181.0, 2019, 2, new Action());
+		Movie m3 = Movie.of("Zoolander", "Ben Stiller, Owen Wilson", 90.0, 1998, 1, new Comedy());
+		Movie m4 = Movie.of("The Shawshank Redemption", "Tim Robbins, Morgan Freeman", 142.0, 1994, 5, new Drama());
+		Movie m5 = Movie.of("Forrest Gump", "Tom Hanks", 142.5, 1994, 3, new Comedy());
+		Movie m6 = Movie.of("The Dark Knight", "Christian Bale, Heath Ledger", 152.0, 2008, 5, new Rock());
 
 		// dummy books
-		Book b1 = new Book("The Da Vinci Code", "Dan Brown", 2003, 1, 689, new Fantasy());
-		Book b2 = new Book("Harry Potter and the Deathly Hallows", "J. K. Rowlings", 2007, 3, 607, new Fantasy());
-		Book b3 = new Book("Show Dog", "Phil Knight", 2016, 5, 399, new Autobiography());
-		Book b4 = new Book("The Fellowship of the Ring", "J. R. R. Tolkien", 1954, 2, 423, new Fantasy());
-		Book b5 = new Book("Surely You're Jocking, Mr. Feynman", "Richard P. Feynman", 1985, 5, 356, new Autobiography());
-		Book b6 = new Book("Oliver Twist", "Charles Dickens", 1838, 5, 608, new Rock());
+		Book b1 = Book.of("The Da Vinci Code", "Dan Brown", 2003, 1, 689, new Fantasy());
+		Book b2 = Book.of("Harry Potter and the Deathly Hallows", "J. K. Rowlings", 2007, 3, 607, new Fantasy());
+		Book b3 = Book.of("Show Dog", "Phil Knight", 2016, 5, 399, new Autobiography());
+		Book b4 = Book.of("The Fellowship of the Ring", "J. R. R. Tolkien", 1954, 2, 423, new Fantasy());
+		Book b5 = Book.of("Surely You're Jocking, Mr. Feynman", "Richard P. Feynman", 1985, 5, 356, new Autobiography());
+		Book b6 = Book.of("Oliver Twist", "Charles Dickens", 1838, 5, 608, new Rock());
 
 		// create subscribers for testing purpose
 		// user A12345
